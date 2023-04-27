4da0b599d5292a30e3038a4f6da25136cc34f84c
BilguunLeap3 <bilguunleap3@iMac2021-02s-iMac.local>
2023-04-27 10:19:56 +0800

    feat: 🎸 added icons

---
+++ src/components/Icon/hamburgermenu.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Svg, {Path} from 'react-native-svg';
+export const SvgHamburgermenu = ({ fill, width, height, ...props }) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    fill="none"
+    {...props}>
+    <Path
+      fill={fill}
+      fillRule="evenodd"
+      d="M25.333 10.667a.8.8 0 0 1-.8.8H7.467a.8.8 0 0 1 0-1.6h17.066a.8.8 0 0 1 .8.8ZM25.333 16a.8.8 0 0 1-.8.8H7.467a.8.8 0 0 1 0-1.6h17.066a.8.8 0 0 1 .8.8ZM25.333 21.334a.8.8 0 0 1-.8.8H7.467a.8.8 0 0 1 0-1.6h17.066a.8.8 0 0 1 .8.8Z"
+      clipRule="evenodd"
+    />
+  </Svg>
+);
+
---
+++ src/components/Icon/music.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Svg, {Rect, Path} from 'react-native-svg';
+export const SvgMusic = ({ fill, width, height, ...props }) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    fill="none"
+    {...props}>
+    <Rect width={40} height={40} fill={fill} rx={20} />
+    <Path
+      stroke="#444"
+      d="M17.7 25.367a2.3 2.3 0 1 1-4.6 0 2.3 2.3 0 0 1 4.6 0ZM26.9 23.833a2.3 2.3 0 1 1-4.6 0 2.3 2.3 0 0 1 4.6 0ZM17.7 25.366v-8.433M26.9 23.833V15.4"
+    />
+    <Path
+      stroke="#444"
+      strokeLinecap="round"
+      d="M22.863 13.679 19.797 14.7c-1.012.337-1.518.506-1.808.907-.289.402-.289.936-.289 2.002V20l9.2-3.067v-.345c0-1.942 0-2.912-.637-3.371-.637-.46-1.558-.152-3.4.462Z"
+    />
+  </Svg>
+);
+
