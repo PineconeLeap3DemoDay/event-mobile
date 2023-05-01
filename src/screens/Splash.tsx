9e599064f4bc3d07c5a92d557e1fa64c426a2df8
BilguunLeap3 <bilguunleap3@iMac2021-02s-iMac.local>
2023-05-01 09:52:49 +0800

    fix: 🐛 fixed splash screen animation
    used react native Animated instead of react native reanimated

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,60 +1,43 @@
-import { Text, SafeAreaView, Animated as RnAnimated } from 'react-native'
+import { Text, SafeAreaView, Animated as RnAnimated, Easing } from 'react-native'
 import React, { useEffect, useRef } from 'react'
-import Animated, {
-    useSharedValue,
-    useAnimatedStyle,
-    withTiming,
-} from 'react-native-reanimated';
+
 import { colors } from '../../colors';
-const SIZE = 50.0;
+import { responsiveWidth } from '../utils/width';
+const SIZE = responsiveWidth(50.0);
 
 export default function Splash({setLoading}: any) {
-    const x1 = useSharedValue(160);
-    const y1 = useSharedValue(40);
-    const x2 = useSharedValue(0);
-    const y2 = useSharedValue(150);
-    const x3 = useSharedValue(250);
-    const y3 = useSharedValue(150);
-    const x4 = useSharedValue(150);
-    const y4 = useSharedValue(-140);
-    let opacity = useRef(new RnAnimated.Value(-200)).current;
-
-    const topItem = useAnimatedStyle(() => {
-        return {
-            transform: [
-                { translateX: x1.value },
-                { translateY: y1.value },
-            ],
-        };
-    }, []);
-    const leftItem = useAnimatedStyle(() => {
-        return {
-            transform: [
-                { translateX: x2.value },
-                { translateY: y2.value },
-            ],
-        };
-    }, []);
-    const rightItem = useAnimatedStyle(() => {
-        return {
-            transform: [
-                { translateX: x3.value },
-                { translateY: y3.value },
-            ],
-        };
-    }, []);
-
+    let leftItem = useRef(new RnAnimated.ValueXY({x: 0, y:150})).current;
+    let textItem = useRef(new RnAnimated.Value(-200)).current;
+    let rightItem = useRef(new RnAnimated.ValueXY({x: 250, y:160})).current;
+    let topItem = useRef(new RnAnimated.ValueXY({x: 160, y:40})).current;
+    
     useEffect(() => {
-        x1.value = withTiming(120, { duration: 1000 });
-        y1.value = withTiming(170, { duration: 2000 });
-        x2.value = withTiming(100, { duration: 1000 });
-        y2.value = withTiming(150, { duration: 3000 });
-        x3.value = withTiming(145, { duration: 1000 });
-        y3.value = withTiming(90, { duration: 3000 });
-        x4.value = withTiming(145, { duration: 2000 });
-        y4.value = withTiming(70, { duration: 2000 });
+        RnAnimated.timing(
+            topItem, {
+            toValue: {x: 120, y: 170},
+            useNativeDriver: true,
+            delay: 1500,
+            easing: Easing.linear
+        }
+        ).start();
+        RnAnimated.timing(
+            rightItem, {
+            toValue: {x: 145, y: 90},
+            useNativeDriver: true,
+            delay: 1500,
+            easing: Easing.linear
+        }
+        ).start();
+        RnAnimated.timing(
+            leftItem, {
+            toValue: {x: 100, y: 150},
+            useNativeDriver: true,
+            delay: 1500,
+            easing: Easing.linear
+        }
+        ).start();
         RnAnimated.spring(
-            opacity, {
+            textItem, {
             toValue: 50,
             friction: 5,
             useNativeDriver: true,
@@ -62,33 +45,39 @@ export default function Splash({setLoading}: any) {
         }
         ).start();
         setTimeout(() => {
-            setLoading(false);
-        },3000)
+            setLoading(false)
+        }, 3000);
     }, []);
 
     return (
         <SafeAreaView >
-            <Animated.View
+            <RnAnimated.View
                 style={[
                     { height: SIZE, width: SIZE, backgroundColor: colors.secondary, borderTopLeftRadius: 50, borderTopRightRadius: 50, borderBottomLeftRadius: 40, borderBottomRightRadius: 40 },
-                    topItem,
+                    {transform: [{
+                        translateX: topItem.x,
+                    }, {translateY: topItem.y}]}
                 ]}
             />
-            <Animated.View
+            <RnAnimated.View
                 style={[
                     { height: SIZE, width: SIZE, backgroundColor: '#FBEB5A', borderTopLeftRadius: 50, borderTopRightRadius: 50, borderBottomLeftRadius: 40, borderBottomRightRadius: 40 },
-                    leftItem,
+                    {transform: [{
+                        translateX: leftItem.x,
+                    }, {translateY: leftItem.y}]}
                 ]}
             />
-            <Animated.View
+            <RnAnimated.View
                 style={[
                     { height: SIZE, width: SIZE, backgroundColor: '#FCEFF8', borderTopLeftRadius: 50, borderTopRightRadius: 50, borderBottomLeftRadius: 40, borderBottomRightRadius: 40 },
-                    rightItem,
+                    {transform: [{
+                        translateX: rightItem.x,
+                    }, {translateY: rightItem.y}]}
                 ]}
             />
             <RnAnimated.View style={{
                 transform: [
-                    { translateY: opacity }, { translateX: 150 }
+                    { translateY: textItem }, { translateX: 150 }
                 ]
             }}>
                 <Text style={{ fontWeight: 'bold', fontSize: 25 }}>UB Events</Text>
